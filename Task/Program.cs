d184f0491efb82d08545592424dbc148a15e2cc6
Mina Youssef <minayoussef9876@gmail.com>
2025-07-18 15:25:53 +0300

    Add project files.

---
+++ Task/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task
+{
+    internal class Program
+    {
+        static Zoo GetZooWithMaxAnimalCount(Zoo[] zoos)
+        {
+            Zoo zooWithMaxAnimalCount = new Zoo();
+            int maxAnimalCount = zoos[0].AnimalCount;
+
+            for (int i = 1; i < zoos.Length; i++)
+            {
+                if (zoos[i].AnimalCount > maxAnimalCount)
+                    zooWithMaxAnimalCount = zoos[i];
+            }
+            return zooWithMaxAnimalCount;
+        }
+
+        static void Main(string[] args)
+        {
+            Zoo[] zoos = new Zoo[3];
+
+            Console.WriteLine("Enter 3 Zoos \n");
+            for (int i = 0; i < zoos.Length; i++)
+            {
+                Console.WriteLine($"Enter Zoo number {i+1}: ");
+                
+                Console.Write("Enter its name: ");
+                zoos[i].Name = Console.ReadLine();
+
+                Console.Write("Enter its Animal Count: ");
+                if(int.TryParse(Console.ReadLine(), out int animalCount))
+                {
+                    zoos[i].AnimalCount = animalCount;
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(GetZooWithMaxAnimalCount(zoos));
+        }
+    }
+}
---
+++ Task/Zoo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    internal struct Zoo
+    {
+        private string name;
+        private int animalCount;
+
+        public string Name
+        {
+            set
+            {
+                if (value is null || value.Length == 0)
+                    return;
+
+                name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+
+        public int AnimalCount
+        {
+            set
+            {
+                if(value < 0)
+                    return;
+
+                animalCount = value;
+            }
+            get
+            {
+                return animalCount;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Zoo name: {Name}, Animal count: {AnimalCount}";
+        }
+    }
+}
