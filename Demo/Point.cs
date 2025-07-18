f17bfb8b0010d67e12e11ca4bdf949f14cf9c75f
Mina Youssef <minayoussef9876@gmail.com>
2025-07-18 16:30:02 +0300

    V01 Struct

---
+++ Demo/Point.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Demo
+{
+    internal struct Point
+    {
+        //public int x;
+        //public int y;
+
+        // Constructor is special method
+        // 1. Same name as struct
+        // 2. No return type
+
+        // CLR will generate paramterless constructor.
+        // This constructor will initialize the attributes with default values
+
+        // Constructor
+        //public Point()
+        //{
+
+        //}
+
+
+
+        // .NET 6.0   C# 10.0    // paramaterless ctor can be created but it must initialize attributes
+        //public Point()
+        //{
+        //    x = 8;
+        //    y = 8;
+        //}
+
+        // in .NET 7.0 a parameterless ctor can be created without initializing attributes
+
+        //public Point(int x, int y)
+        //{
+        //    this.x = x; 
+        //    this.y = y;
+        //}
+
+        //public void PrintPoint()
+        //{
+        //    Console.WriteLine($"({x}, {y})");
+        //}
+
+        //public override string ToString()
+        //{
+        //    return $"({x}, {y})";
+        //}
+    }
+}
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,57 @@
 ﻿namespace Demo
 {
+    // 1. Class
+    // 2. Struct
+    // 3. Interface
+    // 4. Enum
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            #region V01 Struct
+            // Struct: Value type - STACK
+
+            // Employee
+            // Id, Name, Salary
+
+
+            // Point
+            //Point p01;
+            // Declare of object of type Point
+            // Allocate uninialized 8 bytes at stack
+
+            //Console.WriteLine(p01.x);      // error: Unassigned variable
+            //Console.WriteLine(p01.y);      // error: Unassigned variable
+
+            //p01.x = 12;
+            //p01.y = 12;
+
+            //Console.WriteLine(p01.x);
+            //Console.WriteLine(p01.y);
+
+
+            //Point p02 = new Point();
+            //// new: used for selection of constructor
+
+            //Console.WriteLine(p02.x);       // 0
+            //Console.WriteLine(p02.y);       // 0
+
+
+            //Point p03 = new Point(1, 2);
+
+            //Console.WriteLine(p03.x);        // 1
+            //Console.WriteLine(p03.y);        // 2
+
+            //Console.WriteLine(p03);       
+
+            //p03.PrintPoint();
+
+            //Console.WriteLine(p03.ToString());
+
+            //Console.WriteLine(p03.ToString());
+            //Console.WriteLine(p03);
+            #endregion
         }
     }
 }
