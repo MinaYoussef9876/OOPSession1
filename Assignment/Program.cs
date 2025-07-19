e57d85dba3c2eb50ea61e280117659b4d65709ff
Mina Youssef <minayoussef9876@gmail.com>
2025-07-19 17:14:28 +0300

    Part 01  Q01

---
+++ Assignment/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part01
+
+
+            #region Q01
+            // 1.Create a struct called "Point" to represent a 2D point with properties "X" and "Y". 
+            // Write a C# program that takes two points as input from the user and calculates the distance between them.
+            //Console.WriteLine("Enter 2 Points");
+
+            //Console.WriteLine("Enter the First Point");
+            //Point p01 = AssignPoint();
+
+            //Console.WriteLine("Enter the second Point");
+            //Point p02 = AssignPoint();
+
+            //double distance = CalculateDistance(p01, p02);
+            //Console.WriteLine($"Distance between the 2 points = {distance}");
+            
+
+
+
+            //static Point AssignPoint()
+            //{
+            //    Point p = new Point();
+            //    p.X = ReadPointCoordinate('x');
+            //    p.Y = ReadPointCoordinate('y');
+            //    return p;
+            //}
+
+            //static double ReadPointCoordinate(char c)
+            //{
+            //    double coordinateValue;
+            //    bool flag;
+            //    do
+            //    {
+            //        flag = false;
+            //        Console.Write($"Enter {c} of point:  ");
+            //        flag = double.TryParse(Console.ReadLine(), out double value);
+            //        coordinateValue = value;
+            //    } while (!flag);
+            //    return coordinateValue;
+            //}
+
+            //static double CalculateDistance(Point p1, Point p2)
+            //{
+            //    double dx = p2.X - p1.X;
+            //    double dy = p2.Y - p1.Y;
+            //    double result = Math.Sqrt(dx * dx + dy * dy);
+            //    return result;
+            //}
+            #endregion
+
+
+            #endregion
+        }
+    }
+}
